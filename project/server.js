742648e7ba4c0a9df100da1cf3d0a16b03ca22a7
draculafati <fmohammedzafar@gmail.com>
2025-07-28 11:25:28 +0530

    server.js

--- project/server.js
+++ project/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-require('dotenv').config();
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
